609eda7c4f2d8a517b87564db9f0df37a22472c1
jackz070 <95173354+jackz070@users.noreply.github.com>
2022-12-05 14:17:15 +0100

    Add test comment to pokedex.jsx

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// TEST COMMENT
 import PokedexSinglePokemonWrapper from "./PokedexSinglePokemonWrapper";
 import FullScreenLoading from "../Loaders/FullScreenLoading";
 
