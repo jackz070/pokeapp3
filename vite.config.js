17ca519144c369ea1cc25ccc4353e1b6d58e96d8
jackz070 <95173354+jackz070@users.noreply.github.com>
2022-12-14 15:38:25 +0100

    WIP gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy-demo/",
+  // below base line allows the gh-pages deploy to work correcty per vite-deploy-demo guidelines
+  base: "/pokeapp3/",
   // mkcert allows me to run dev server over https, which is required for auth0 which needs to run from "secure web origin", if its not the provider throws an error
 });
