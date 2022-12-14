8d974b5a1cd54514a31d81435643ba7d93a41bc9
jackz070 <95173354+jackz070@users.noreply.github.com>
2022-12-14 15:20:58 +0100

    Updated base in vite.config.js per vite-deply-demo guidelines

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy-demo/",
   // mkcert allows me to run dev server over https, which is required for auth0 which needs to run from "secure web origin", if its not the provider throws an error
 });
