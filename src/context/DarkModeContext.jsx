add0919f240464d0972f23ac951d40e4478c1cfa
jackz070 <95173354+jackz070@users.noreply.github.com>
2023-02-23 17:59:40 +0100

    Dark mode context fix

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const DarkModeContext = React.createContext();
 DarkModeContext.displayName = "DarkModeContext";
 
-// TODO now that changes are visible make it work: persist chosen theme, check system preferences
 const DarkModeContextProvider = (props) => {
   const [darkMode, setDarkMode] = React.useState(localStorage.theme === "dark");
 
